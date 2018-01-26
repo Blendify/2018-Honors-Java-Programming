1848f725b29f1b0ae1245251e691082a618c7cab
Aaron Carlisle <carlisle.aaron00@gmail.com>
2018-01-26 14:25:07 -0500

    Style

--- Helllo World/src/Helllo_World.java
+++ Helllo World/src/Helllo_World.java
@@ -7,15 +7,15 @@
  * Class: helloWorld
  *
  * Developer: Aaron Carlisle
+ *
  * Description:
  * This program displays the developer's name on the screen.
  */
  
 public class Helllo_World {
-    
+
     public static void main(String[] args) {
-    	
-    	// TODO, add your application code
+
     	System.out.println("Aaron Carlisle says Hello World!");
     }
 }
