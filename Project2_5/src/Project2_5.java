38b3484cd87c4c4237432addf4e719264bdbf431
Aaron Carlisle <carlisle.aaron00@gmail.com>
2018-01-30 11:17:26 -0500

    Begin projecr 2.5

---
+++ Project2_5/src/Project2_5.java
@@ -0,0 +1,25 @@
+/**
+ * @(#)Project2_5.java
+ *
+ * Project2_5 application
+ *
+ * @author 
+ * @version 1.00 2018/1/30
+ */
+
+import TerminalIO.KeyboardReader;
+
+public class Project2_5 {
+    
+    public static void main(String[] args) {
+    	KeyboardReader reader = new KeyboardReader();
+    	double mass;
+    	double velocity;
+    	
+    	System.out.print("Enter the object's mass: ");
+    	mass = reader.readDouble();
+
+    	System.out.print("Enter the object's mass: ");
+    	mass = reader.readDouble();
+    }
+}
