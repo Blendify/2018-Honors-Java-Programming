b56b494a5367aeede94215b1b910c9eff9e9a3c0
Aaron Carlisle <carlisle.aaron00@gmail.com>
2018-01-30 11:06:44 -0500

    Project 2.2

---
+++ Project2_2/src/Project2_2.java
@@ -0,0 +1,26 @@
+/**
+ * @(#)Project2_2.java
+ *
+ * Project2-2 application
+ *
+ * @author Aaron Carlisle
+ * @version 1.00 2018/1/30
+ */
+
+public class Project2_2 {
+
+    public static void main(String[] args) {
+
+    	System.out.println("   *****************");
+    	System.out.println("  *  *************  *");
+    	System.out.println("  * *             * *");
+    	System.out.println("   * *           * *");
+    	System.out.println("    * *  YIELD  * *");
+    	System.out.println("     * *       * *");
+    	System.out.println("      * *     * *");
+    	System.out.println("       * *   * *");
+    	System.out.println("        * * * *");
+    	System.out.println("         * * *");
+    	System.out.println("           *");
+    }
+}
