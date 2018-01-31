e964e46732bb862ab8069a9eb0465a67bc5b6e52
Aaron Carlisle <carlisle.aaron00@gmail.com>
2018-01-31 10:18:26 -0500

    Begin 2.6

---
+++ Project2_6/src/Project2_6.java
@@ -0,0 +1,22 @@
+/**
+ * @(#)Project2_6.java
+ *
+ * Project2_6 application
+ *
+ * @author Aaron Carlisle
+ * @version 1.00 2018/1/31
+ */
+
+import TurtleGraphics.StandardPen;
+ 
+public class Project2_6 {
+    public static void main(String[] args) {
+    	StandardPen pen = new StandardPen();
+    	
+    	pen.up();
+    	pen.move(25);
+    	pen.down();
+    	pen.move(50);
+
+    }
+}
