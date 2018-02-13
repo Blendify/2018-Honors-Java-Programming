e5ec7fafd8f40797751dd2dd271dc2f156b9f2b9
Aaron Carlisle <carlisle.aaron00@gmail.com>
2018-02-13 10:01:32 -0500

    More work on WeeklyPay

--- CalcWeeklyPay/src/CalcWeeklyPay.java
+++ CalcWeeklyPay/src/CalcWeeklyPay.java
@@ -21,17 +21,42 @@ public class CalcWeeklyPay {
 	final double OVER_TIME_PAY = 1.5;
 
 	double HourlyWage;
-	double NormalHours;
-	double OvertimeHours;	
+	double MondayNormalHours, TuesdayNormalHours,
+	       WednesdayNormalHours, ThursdayNormalHours,
+	       FridayNormalHours;
+	double MondayOvertimeHours, TuesdayOvertimeHours,
+	       WednesdayOvertimeHours, ThursdayOvertimeHours,
+	       FridayOvertimeHours;	
 	double WeeklyPay;
 	
 	// Ask for Wage and Hours
 	HourlyWage = reader.readDouble("Input the employe's hourly wage: ");
-	NormalHours = reader.readDouble("Input the amount of normal hours the employes worked: ");
-    OvertimeHours = reader.readDouble("Input the amount of overtime hours the employes worked: ");
+	
+	// Ask for normal hours worked per day
+	System.out.println("Input the number of normal hours worked for each of the following days:");
+	MondayNormalHours    = reader.readDouble("Monday: ");
+	TuesdayNormalHours   = reader.readDouble("Tuesday: ");
+	WednesdayNormalHours = reader.readDouble("Wednesday: ");
+	ThursdayNormalHours  = reader.readDouble("Thursday: ");
+	FridayNormalHours    = reader.readDouble("Friday: ");
+
+	// Ask for overtime hours worked per day
+	System.out.println("Input the number of Overtime hours worked for each of the following days:");
+	MondayOvertimeHours    = reader.readDouble("Monday: ");
+	TuesdayOvertimeHours   = reader.readDouble("Tuesday: ");
+	WednesdayOvertimeHours = reader.readDouble("Wednesday: ");
+	ThursdayOvertimeHours  = reader.readDouble("Thursday: ");
+	FridayOvertimeHours    = reader.readDouble("Friday: ");
     
     // Calculate the weekly pay
-    WeeklyPay = (NormalHours * HourlyWage) + (OvertimeHours * HourlyWage * OVER_TIME_PAY);
+    WeeklyPay = ((MondayNormalHours + TuesdayNormalHours +
+	             WednesdayNormalHours + ThursdayNormalHours,
+	             FridayNormalHours) * HourlyWage) + 
+	             
+	             // calculate Overtime pay
+	             ((MondayOvertimeHours, TuesdayOvertimeHours,
+	       		 WednesdayOvertimeHours, ThursdayOvertimeHours,
+	     		 FridayOvertimeHours)) * (HourlyWage * OVER_TIME_PAY);
 
     System.out.println("The employe's weekly pay is: $" + WeeklyPay);
 
