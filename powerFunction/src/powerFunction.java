ef7c09ba69a0d0ae02d76991895e26c642af8c0a
Aaron Carlisle <carlisle.aaron00@gmail.com>
2018-03-01 13:33:04 -0500

    PowerFunction: Get mostly working
    Still needs to work top meet the requirements

--- powerFunction/src/powerFunction.java
+++ powerFunction/src/powerFunction.java
@@ -3,8 +3,10 @@
  *
  * powerFunction application
  *
- * @author 
- * @version 1.00 2018/2/28
+ * Discription: This program calculats all positive powers up to a defined power
+ *
+ * @author Aaron Carlisle
+ * @version 1.00 2018/3/1
  */
 
 import TerminalIO.KeyboardReader; // Used for user input
@@ -14,14 +16,33 @@ public class powerFunction {
     public static void main(String[] args) {
     	KeyboardReader reader = new KeyboardReader();
     	
-    	double base, exponent;
+    	System.out.println("This program calculats all positive powers up to a defined power");
     	
-    	base       = reader.readDouble("Enter the base (x^n): ");
-    	exponent   = reader.readDouble("Enter the highest exponent (" + base +"^x): ");
+    	double base = reader.readDouble("Enter the base (x^n): ");
+    	int exponent = reader.readInt("Enter the highest exponent (" + base +"^x): ");
     	
-    	while (exponent >= 1)
-    		System.out.println(Math.pow(base, exponent));
+    	while (exponent != 1.0) {
+			System.out.println(Math.pow(base, exponent));
     		exponent--;
-	
+    	}
     }
 }
+
+/*
+--------------------Configuration: powerFunction - JDK version 1.6.0_14 <Default> - <Default>--------------------
+This program calculats all positive powers up to a defined power
+Enter the base (x^n): 2.5
+Enter the highest exponent (2.5^x): 10
+9536.7431640625
+3814.697265625
+1525.87890625
+610.3515625
+244.140625
+97.65625
+39.0625
+15.625
+6.25
+
+Process completed.
+ */
+ 
