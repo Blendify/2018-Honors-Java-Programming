dd976e02e5bd8089a44f4ae315bb49e506b23a29
Aaron Carlisle <carlisle.aaron00@gmail.com>
2018-04-11 10:23:00 -0400

    CLeanup

--- TestRational/src/Rational.java
+++ TestRational/src/Rational.java
@@ -10,22 +10,22 @@
 public class Rational {
 	private int numerator;
 	private int denominator;
-	
+
 	private int temp_numer1, temp_denom1, temp_numer2, temp_denom2;
-	
+
 	/* CONSTRUCTORS */
     public Rational() {
     // DEAFULT: create a fraction 0/1
     	numerator = 0;
     	denominator = 1;
     }
-    
+
     public Rational(int numer, int denom) {
     // Create a fraction with client defined values
     	numerator = numer;
     	denominator = denom;
     }
-    
+
     /* METHODS */
     public void add(Rational b){
     //Adds two rationals
@@ -34,7 +34,7 @@ public class Rational {
     	this.denominator = temp_denom1;
 		this.simplify();
     }
-    
+
     public void subtract(Rational b){
     //Subtracts two rationals
 		this.common_factor(b);
@@ -50,7 +50,7 @@ public class Rational {
 		this.denominator = temp_denom1 * temp_denom2;
 		this.simplify();
 	}
-    
+
     public void divide(Rational b){
 	//Divides two fractions
 	   	this.common_factor(b);
@@ -58,14 +58,14 @@ public class Rational {
 		this.denominator = temp_denom1 * temp_numer2;
 		this.simplify();
 	}
-	
+
 	private void common_factor(Rational b){
-		temp_numer1 = this.numerator   * b.denominator;
+			temp_numer1 = this.numerator   * b.denominator;
     	temp_denom1 = this.denominator * b.denominator;
     	temp_numer2 = b.numerator   * this.denominator;
     	temp_denom2 = b.denominator * this.denominator;
 	}
-	
+
 	public void reciprocal(){
 		temp_numer1 = this.numerator;
 		temp_denom1 = this.denominator;
@@ -83,7 +83,7 @@ public class Rational {
     		}
    		}
     }
-    
+
     public Boolean compare(Rational b){
     	this.common_factor(b);
     	if ((temp_numer1 == temp_numer2) && (temp_denom1 == temp_denom2))
