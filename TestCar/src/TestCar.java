a52cc00c54926c67529aadd3abc2e64b07e5b42d
Aaron Carlisle <carlisle.aaron00@gmail.com>
2018-04-12 13:52:08 -0400

    Begin TestCar

---
+++ TestCar/src/TestCar.java
@@ -0,0 +1,33 @@
+/**
+ * @(#)TestCar.java
+ *
+ * TestCar application
+ *
+ * @author Aaron Carlisle
+ * @version 1.00 2018/4/12
+ */
+
+import TurtleGraphics.*;
+import TerminalIO.*
+
+public class TestCar {
+    
+    public static void main(String[] args) {
+    	KeyboardReader reader = new KeyboardReader();
+    	double x, y;
+    	//double radius;
+    	
+    	x = reader.readDouble("Intial x position: ");
+    	y = reader.readDouble("Intial x position: ");
+    	Car lambo = new car(x, y);
+    	lambo.draw();
+    	
+    	while(true){
+    		x = reader.readDouble("New x position: ");
+    		y = reader.readDouble("New y position: ");
+    		lambo.erase();
+    		lambo.move();
+    		lambo.draw();
+    	}
+    }
+}
