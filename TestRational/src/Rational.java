8def43fce00c486878d0999afcedc0929da46260
Aaron Carlisle <carlisle.aaron00@gmail.com>
2018-04-17 09:29:59 -0400

    Hopefully fix errors in rationalality

--- TestRational/src/Rational.java
+++ TestRational/src/Rational.java
@@ -30,35 +30,29 @@ public class Rational {
     public Rational add(Rational b){
     //Adds two rationals
     	this.common_factor(b);
-		
+
 		return new Rational(temp_numer1 + temp_numer2, temp_denom1).simplify(this);
     }
 
     public Rational subtract(Rational b){
     //Subtracts two rationals
 		this.common_factor(b);
-		
+
 		return new Rational(temp_numer1 - temp_numer2, temp_denom1).simplify(this);
     }
 
     public Rational multiply(Rational b){
-	   	this.common_factor(b);
-		Rational c = new Rational (temp_numer1 * temp_numer2, temp_denom1 * temp_denom2);
-		return c.simplify(c);
+		//Multiplies two fractions
+	  	this.common_factor(b);
+			Rational c = new Rational (temp_numer1 * temp_numer2, temp_denom1 * temp_denom2);
+			return c.simplify(c);
 	}
 
     public Rational divide(Rational b){
-	//Divides two fractions
+		//Divides two fractions
 	   	this.common_factor(b);
-		
-		return new Rational(temp_numer1 * temp_denom2, temp_denom1 * temp_numer2).simplify(this);
-	}
 
-	private void common_factor(Rational b){
-		temp_numer1 = this.numerator   * b.denominator;
-    	temp_denom1 = this.denominator * b.denominator;
-    	temp_numer2 = b.numerator   * this.denominator;
-    	temp_denom2 = b.denominator * this.denominator;
+		return new Rational(temp_numer1 * temp_denom2, temp_denom1 * temp_numer2).simplify(this);
 	}
 
 	public Rational reciprocal(Rational b){
@@ -68,12 +62,11 @@ public class Rational {
 		return new Rational(temp_denom1, temp_numer1);
 	}
 
-    public void simplify(){
+    public Rational simplify(Rational a){
     //Simplifies a fraction to its simplist form
-    	for (int i = Math.min(this.numerator, this.denominator); i >= 2; i--){
-     		if ((this.numerator % i == 0) && (this.denominator % i == 0)){
-    			this.numerator /= i;
-    			this.denominator /= i;
+    	for (int i = Math.min(a.numerator, a.denominator); i >= 2; i--){
+     		if ((a.numerator % i == 0) && (a.denominator % i == 0)){
+    			return Rational(a.numerator /= i, a.denominator /= i);
     		}
    		}
     }
@@ -93,4 +86,11 @@ public class Rational {
     	str = numerator + "\n--\n" + denominator + "\n";
     	return str;
     }
+
+		private void common_factor(Rational b){
+			temp_numer1 = this.numerator   * b.denominator;
+				temp_denom1 = this.denominator * b.denominator;
+				temp_numer2 = b.numerator   * this.denominator;
+				temp_denom2 = b.denominator * this.denominator;
+		}
 }
