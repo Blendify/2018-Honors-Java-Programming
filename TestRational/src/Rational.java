a7efdfa4fc3da1a7c67f5c73c3922e37cfae7259
Aaron Carlisle <carlisle.aaron00@gmail.com>
2018-04-18 10:43:43 -0400

    Fix simplify and begin on mixed

--- TestRational/src/Rational.java
+++ TestRational/src/Rational.java
@@ -10,6 +10,7 @@
 public class Rational {
 	private int numerator;
 	private int denominator;
+	private int wholeNumber = 0;
 
 	private int temp_numer1, temp_denom1, temp_numer2, temp_denom2;
 
@@ -26,6 +27,13 @@ public class Rational {
     	denominator = denom;
     }
 
+    public Rational(int numer, int denom, int whole) {
+    // Create a fraction with defined values
+    	numerator = numer;
+    	denominator = denom;
+    	wholeNumber = whole;
+    }
+	
     /* METHODS */
     public Rational add(Rational b){
     //Adds two rationals
@@ -72,28 +80,30 @@ public class Rational {
     //Simplifies a fraction to its simplist form
     	boolean is_negative;
 
+    	if (this.numerator == 0){
+			this.denominator = 1;
+    	}
+    	if ((this.numerator < 0) || (this.denominator < 0)){
+    		is_negative = true;
+			this.numerator = Math.abs(this.numerator);
+			this.denominator = Math.abs(this.denominator);
+    	} else {
+    		is_negative = false;
+    	}
     	for (int i = Math.min(this.numerator, this.denominator); i >= 2; i--){
-    		if (this.numerator == 0){
-    			System.out.println("Numerator is 0");
-				this.denominator = 1;
-    			break;
-    		}
-    		if ((this.numerator < 0) || (this.denominator < 0)){
-    			is_negative = true;
-				Math.abs(this.numerator);
-				Math.abs(this.denominator);
-    		} else {
-    			is_negative = false;
-    		}
      		if ((this.numerator % i == 0) && (this.denominator % i == 0)){
     			this.numerator /= i;
     			this.denominator /= i;
     		}
+    	}
     	if (is_negative == true)
     		this.numerator *= -1;
-   		}
     }
 
+	public Rational toMixed(Rational B){
+		Rational c = new Rational(temp_denom1, temp_numer1);
+		return c;
+	}
     public Boolean compare(Rational b){
    	//Compares two rationals for equality
     	this.common_factor(b);
