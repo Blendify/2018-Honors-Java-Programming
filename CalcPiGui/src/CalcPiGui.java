ebae9ab7bfd9aa7109d7c9f4ba70e58fbbd5ebde
Aaron Carlisle <carlisle.aaron00@gmail.com>
2018-04-26 14:13:18 -0400

    Fork on CalcPiGui

--- CalcPiGui/src/CalcPiGui.java
+++ CalcPiGui/src/CalcPiGui.java
@@ -10,6 +10,21 @@
 import javax.swing.*;
 import BreezySwing.*;
 
+public double Leibniz (int i){
+	double pi = 1.0; // begin with 1 to so we can subtract from it first
+	double denominator = 1.0;
+
+    for (int loopCount = 0; loopCount < i; loopCount++) {
+    	denominator = (denominator + 2.0);
+    	if ((loopCount % 2) != 0) // Are we odd or even itererations
+			pi += (1.0 / denominator);
+		else
+			pi -= (1.0 / denominator);
+    }
+    pi *= 4.0;
+	return pi;
+}
+
 public class CalcPiGui extends GBFrame {
 	private JLabel       iterationsLabel;
 	private JLabel       piLabel;
@@ -20,30 +35,16 @@ public class CalcPiGui extends GBFrame {
   public CalcPiGui() {
   	// Constructor
   	iterationsLabel = addLabel("Iterations:",1,1,1,1);
-  	piLabel         = addLabel("PI",         1,1,1,1);
+  	piLabel         = addLabel("PI",         1,2,1,1);
 
-  	iterationsField = addIntegerField   (0,2,1,1,1);
-  	piField         = addDoubleField    (0.0,2,1,1,1);
+  	iterationsField = addIntegerField   (0,  2,1,1,1);
+  	piField         = addDoubleField    (0.0,2,2,1,1);
   	calculateButton = addButton         ("Calculate",3,1,2,1);
   }
 
-	public void buttonCLicked(JButton buttonObj) {
+	public void buttonClicked(JButton buttonObj) {
 	}
 
-    /*
-    public double Leibniz (int i){
-        while (loopCount < i) {
-        	denominator = (denominator + 2.0);
-        	if ((loopCount % 2) != 0) // Are we odd or even itererations
-    			pi += (1.0 / denominator);
-    		else
-    			pi -= (1.0 / denominator);
-        	loopCount += 1;
-        }
-        pi *= 4.0;
-    	renturn pi;
-    }
-    */
     public static void main(String[] args) {
 		CalcPiGui gui = new CalcPiGui();
 		gui.setTitle("Leibniz's Calculation of Pi");
